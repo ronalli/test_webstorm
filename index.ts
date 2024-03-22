2022e6dc8c1b310197e5ce00354e87467f425200
Nikita Khakim <khakimov.nikita.vch@gmail.com>
2024-03-22 20:38:05 +0200

    start webstorm - 1

--- index.ts
+++ index.ts
@@ -1,9 +1,16 @@
 const a = 'd';
 console.log(a);
+interface IUser {
+    name: string,
+    lastname: string,
+    age: number
+}
 
-const user = {
+const user: IUser = {
     name: 'Bob',
     lastname: 'Flex',
     age: 25
 }
 console.log(user);
+
+
