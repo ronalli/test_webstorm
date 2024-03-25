47cb13d0d110850d17c8f51bda4eef3543e6d9b5
Nikita Khakim <khakimov.nikita.vch@gmail.com>
2024-03-25 10:07:45 +0200

    output info - 1

--- index.ts
+++ index.ts
@@ -12,3 +12,15 @@ const user: IUser = {
 for(const [key, value] of Object.entries(user)) {
     console.log(`${key}: ${value}`)
 }
+
+interface Info {
+    [key:string]: number;
+    num: number;
+}
+
+const infoBooks: Info = {
+    page: 1234,
+    num: 45
+}
+
+console.log(print)
